539fa87ad46c273cc98d96b1c13b33a1c1128b9d
Fernanda Fernandes <98698976+Ferfkm@users.noreply.github.com>
2024-11-13 14:57:32 -0300

    projeto estudo

--- perguntas.js
+++ perguntas.js
@@ -1,10 +1,56 @@
 criarCartao(
-    "Programação",
-    "O que é Python",
-    "O Python é uma linguagem de programação."
+    "Análise do Comportamento",
+    "Descreva o que é reforço contínuo e reforço intermitente?",
+    "No CRF, toda resposta é seguida da apresentação de um estímulo reforçador, como por exemplo, quando você compra um carro novo, toda vez que gira a chave, o motor começa a funcionar. Já no esquema de reforçointermitente, algumas respostas são reforçadas e outras não, como por exemplo um carro velho, às vezes ele liga, às vezes ele não liga quando gira achave."
 )
 criarCartao(
-    "Geografia",
-    "Qual a capital da França",
-    "A capital da França é Paris."
+    "Análise do Comportamento",
+    "Qual esquema de reforço dá mais sensação de liberdade?",
+    "Os esquemas de reforço positivo dão mais sensação de liberdade porque incentivam comportamentos sem a presença de estímulos aversivos. Como o comportamento é reforçado por consequências agradáveis, o indivíduo tende a agir de forma mais espontânea e voluntária, sem a pressão de evitar punições ou escapar de situações desagradáveis. Isso promove um ambiente onde a pessoa se sente mais autônoma, já que está sendo motivada por recompensas que ela mesma busca, e não pela necessidade de evitar algo negativo."
 )
+criarCartao(
+    "Análise do Comportamento",
+    "Diferencie o esquema de razão do esquema de intervalo:",
+    "Nos esquemas de razão (FR ou VR), o requisito para a liberação da consequência reforçadora é o número de respostas emitidas, enquanto nos esquemas de intervalo (FI ou VI), o principal requisito é a passagem de certo período de tempo desde o reforçamento da última resposta."
+)
+criarCartao(
+    "Análise do Comportamento",
+    "Qual esquema mantém mais o comportamento do organismo a longo prazo? Intermitente ou contínuo?",
+    " O intermitente devido ao chamado efeito de resistência à extinção, pois o reforço não é apresentado a cada resposta, mas sim de maneira esporádica e imprevisível. Isso faz com que o organismo continue emitindo o comportamento na expectativa de que o reforço eventualmente ocorra."
+)
+criarCartao(
+    "Análise do Comportamento",
+    "O que é controle aversivo?",
+    "Refere-se ao uso de estímulos aversivos para influenciar o comportamento de um organismo, como por exemplo o uso de punição e reforço negativo, pois são aqueles estímulos que o organismo evita ou que causam desconforto, dor ou ansiedade."
+)
+criarCartao(
+    "Análise do Comportamento",
+    "A punição funciona? De que maneira ela pode ser útil?",
+    "Estudos mostram que a punição não tem caráter permanente, ou seja, quando um comportamento não for mais punido, ele pode voltar a ocorrer. Ela pode ser útil quando tenha magnitude significativa, isto é, se uma pessoa foi  punida severamente de modo que não consiga mais se comportar, podendo ter um efeito a longo prazo."
+)
+criarCartao(
+    "Análise do Comportamento",
+    "Quais são os efeitos colaterais da punição?",
+    " O uso da punição como forma de controle do comportamento gera alguns comportamentos de fuga e esquiva, ou seja, tentativa de acessar o reforço que lhe foi negado através de outra forma sem passar pela punição ou emitir o comportamento de forma discriminada, isto é, apenas em situações em que não será punido."
+)
+criarCartao(
+    "Análise do Comportamento",
+    "Explique como o behaviorismo considera as emoções:",
+    "Para o behaviorismo, as emoções, como os pensamentos e sentimentos são comportamentos. O que diferencia o comportamento de correr do comportamento de sentir é a quantidade de observadores."
+)
+criarCartao(
+    "Análise do Comportamento",
+    "Diferencie evento público e evento privado:",
+    "Evento público: acessível a mais de um indivíduo (correr, comer...) e evento privado: acessível apenas ao próprio indivíduo que se comporta (sentimentos, emoções e pensamentos)."
+)
+criarCartao(
+    "Análise do Comportamento",
+    "De que maneira eventos privados podem tornar-se público (indiretamente)?",
+    "Através do comportamento verbal do tacto, pois ele é responsável pela descrição de eventos privados."
+)
+criarCartao(
+    "Análise do Comportamento",
+    "Explique a ansiedade em termos behavioristas:",
+    "A ansiedade pode ocorrer por respostas fisiológicas (comportamento respondente) ou por respostas comportamentais (comportamento operante). Um estímulo sinaliza a chegada de um estímulo aversivo e não há comportamento de fuga ou esquiva possível. A ansiedade é a antecipação de um estímulo perturbador no futuro."
+)
+
